146608efc6a768f50e76f9f3fd90bd4085de5ae8
ChanYeolL <403427693@QQ.COM>
2017-10-06 21:05:58 +0800

    Create Binary Watch.cpp

---
+++ Binary Watch.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public List<String> readBinaryWatch(int num) {
+        List<String> alltime = new ArrayList<>() ;
+        
+        for(int i=0 ;i<12 ;i++){
+            for(int j=0 ;j<60 ;j++){
+                if(Integer.bitCount((i<<6)+j) == num){
+                    alltime.add(String.format("%d:%02d" , i, j)) ;
+                }
+            }
+        }
+        return alltime ;
+    }
+}
